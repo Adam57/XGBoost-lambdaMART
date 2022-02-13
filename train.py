122b728c0f866a0bbc23d08f74ddedfed6c64bd3
Adam57 <qiwangwq@amazon.com>
2022-02-13 03:34:48 -0800

    use xgboost=0.81

--- train.py
+++ train.py
@@ -4,13 +4,19 @@ import xgboost as xgb
 if __name__ == "__main__":
     training_data = xgb.DMatrix('/Users/qiwangwq/dev/data/MQ2008/Fold1/train_dat.txt')
     testing_data = xgb.DMatrix('/Users/qiwangwq/dev/data/MQ2008/Fold1/test_dat.txt')
+    # training_data = xgb.DMatrix('/Users/qiwangwq/dev/Fold1/train_dat.txt')
+    # testing_data = xgb.DMatrix('/Users/qiwangwq/dev/Fold1/test_dat.txt')
+
 
     print(testing_data.num_col())
     print(testing_data.num_row())
 
-    param = {'max_depth':8, 'eta':0.1, 'objective':'rank:pairwise'}
+    # the following are for version 1.5.2
+    # param = {'max_depth':8, 'eta':0.1, 'objective':'rank:pairwise'}
+    # model = xgb.train(param, training_data, num_boost_round=100, evals=[(testing_data, "testing_data")], verbose_eval=True)
 
-    model = xgb.train(param, training_data, num_boost_round=100, evals=[(testing_data, "testing_data")], verbose_eval=True)
+    param = {'max_depth':6, 'eta':0.3, 'silent':0, 'objective':'rank:pairwise', 'num_round':10}
+    model = xgb.train(param, training_data)
 
     dump_list = model.get_dump()
     num_trees = len(dump_list)
