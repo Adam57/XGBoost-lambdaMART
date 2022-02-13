8d9b0a4b4c93f379d4b69ddb6403562eed8f7c1a
Adam57 <qiwangwq@amazon.com>
2022-02-13 02:25:58 -0800

    Add more parameters for training

---
+++ train.py
@@ -0,0 +1,19 @@
+import numpy as np
+import xgboost as xgb
+
+if __name__ == "__main__":
+    training_data = xgb.DMatrix('/Users/qiwangwq/dev/data/MQ2008/Fold1/train_dat.txt')
+    testing_data = xgb.DMatrix('/Users/qiwangwq/dev/data/MQ2008/Fold1/test_dat.txt')
+
+    print(testing_data.num_col())
+    print(testing_data.num_row())
+
+    param = {'max_depth':8, 'eta':0.1, 'objective':'rank:pairwise'}
+
+    model = xgb.train(param, training_data, num_boost_round=100, evals=[(testing_data, "testing_data")], verbose_eval=True)
+
+    dump_list = model.get_dump()
+    num_trees = len(dump_list)
+    print(num_trees)
+
+    model.save_model('/Users/qiwangwq/dev/gbdt-rs/examples/model.json')
